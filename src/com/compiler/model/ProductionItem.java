94c05e97c10fda05de5c717d1f8446009d2331e9
ToSnow <towintersnow@gmail.com>
2022-03-20 21:43:32 +0800

    完成文法的项目类的创建

---
+++ src/com/compiler/model/ProductionItem.java
@@ -0,0 +1,99 @@
+package com.compiler.model;
+
+import java.util.List;
+
+/**
+ * 文法的项目
+ * 如：A->a•b,a/#
+ * */
+public class ProductionItem {
+    public static final String DELIMITER = "•";     //分割符
+    private final Production production;            //项目的产生式
+    private final int delimiterPos;                 //分隔符的位置
+    private final Symbol expect;                    //项目的展望符
+    private final String content;                   //该文法项目的字符串形式
+
+    public ProductionItem(Production production, int delimiterPos, Symbol expect, String content) {
+        this.production = production;
+        this.delimiterPos = delimiterPos;
+        this.expect = expect;
+        this.content = content;
+    }
+
+    /**
+     * 生成项目的字符串形式，并创建文法的项目
+     * @param production    产生式
+     * @param expect        项目的展望符
+     * @param delimiterPos  分隔符的位置
+     * @return              文法的项目
+     * */
+    public static ProductionItem create(Production production, Symbol expect, int delimiterPos){
+        List<Symbol> rightList = production.getRight();
+        StringBuilder stringBuilder = new StringBuilder();
+        //将分隔符插入到项目的字符串中
+        for(int i = 0; i < rightList.size(); ++i){
+            Symbol symbol = rightList.get(i);
+            if(i == delimiterPos){
+                //插入分隔符
+                stringBuilder.append(DELIMITER);
+            }
+            stringBuilder.append(symbol.getContent());
+        }
+        //分隔符在末尾的情况
+        if(delimiterPos == rightList.size()){
+            stringBuilder.append(DELIMITER);
+        }
+        //插入展望符
+        stringBuilder.append(",").append(expect.getContent());
+        //调用构造函数
+        return new ProductionItem(production, delimiterPos, expect, stringBuilder.toString());
+    }
+
+    /**
+     * 生成项目的字符串形式，并创建文法的项目，分割符默认为0
+     * @param production    产生式
+     * @param expect        项目的展望符
+     * @return              文法的项目
+     * */
+    public static ProductionItem create(Production production,Symbol expect){
+        return create(production, expect,0);
+    }
+
+    /**
+     * 根据已有项目，创建该项目的下一个项目
+     * @param productionItem 已有项目
+     * @return               下一个项目
+     * */
+    public static ProductionItem create(ProductionItem productionItem){
+        if(productionItem.delimiterPos >= productionItem.production.getRight().size()){
+            //如果当前项目的分割符已经在项目末尾则创建失败
+            throw new RuntimeException("项目的分隔符已在项目末尾，无法创建");
+        }
+        return create(productionItem.production,productionItem.expect,productionItem.delimiterPos + 1);
+    }
+
+    public static String getDELIMITER() {
+        return DELIMITER;
+    }
+
+    public Production getProduction() {
+        return production;
+    }
+
+    public int getDelimiterPos() {
+        return delimiterPos;
+    }
+
+    public Symbol getExpect() {
+        return expect;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductionItem{" + content + "}";
+    }
+}
