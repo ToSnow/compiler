c57a34e5399c75481bb2854ebad0cdf23719f30b
ToSnow <towintersnow@gmail.com>
2022-03-21 22:08:55 +0800

    完成求First集的操作

--- src/com/compiler/model/Grammar.java
+++ src/com/compiler/model/Grammar.java
@@ -1,5 +1,6 @@
 package com.compiler.model;
 
+import java.io.File;
 import java.util.*;
 
 /**
@@ -12,7 +13,7 @@ public class Grammar {
     private Map<Symbol,FirstSet> firstSetMap;   //文法中所有非终结符对应的First集
     private Map<Symbol,FollowSet> followSetMap; //文法中所有非终结符对应的Follow集
     //以symbol为键，记录以symbol开头的产生式
-    private LinkedHashMap<Symbol, List<Production>> productionMap;
+    private final LinkedHashMap<Symbol, List<Production>> productionMap;
 
     /**
      * 构造函数
@@ -83,6 +84,98 @@ public class Grammar {
         return new Grammar(start, symbolListLinkedHashMap);
     }
 
+    /**
+     * 求解非终结符的 First集合
+     * 求法：
+     * 对于产生式 A -> Xβ
+     * ① 若 X ∈ Vt，则 FIRST(A) = {X}
+     * ② 若 X ∈ Vn，且有产生式X ->δ,δ ∈ Vt，则δ ∈ FIRST(A) (非终结符，将首个终结符加入First集)
+     * ③ 若 X ∈ Vn，X →ε，则ε ∈ FIRST(A) (直接推导)
+     * ④ 若 X → Y1,Y2,……,Yn ∈ Vn,且Y1,Y2,……,Y(i-1)直接推出ε时,
+     *    FIRST(Y1) -ε, FIRST(Y2) -ε, …… ,FIRST(Yi)都包含在FIRST(X)中(无ε)
+     * ⑤ 若 ④ 中所有 Yi 都推出ε时,FIRST(X) = FIRST(Y1) ∪ …… ∪ FIRST(Yn) ∪ {ε}
+     * 反复运用 ② - ⑤ 步骤计算，直到FIRST集合不再增大为止
+     * @param VNSymbol 非终结符
+     * @return         该非终结符对应的First集
+     * */
+    public FirstSet getFirstSet(Symbol VNSymbol){
+        //已经求过则直接返回
+        if(firstSetMap.containsKey(VNSymbol))
+            return firstSetMap.get(VNSymbol);
+        //创建一个First集
+        FirstSet firstSet = new FirstSet(VNSymbol);
+        //获取该非终结符对应的产生式
+        List<Production> productionList = productionMap.get(VNSymbol);
+        List<Production> badProductions = new ArrayList<>();
+        for(Production production : productionList){
+            if(!production.isEpsilon()){
+                //该产生式非空
+                List<Symbol> rightSymbols = production.getRight();
+                if(rightSymbols.contains(VNSymbol)){
+                    /*
+                    * 当产生式右部包含左部符号时，形如A->aAb时
+                    * 则会出现递归调用的死循环的情况，需要特殊处理
+                    * */
+                    badProductions.add(production);
+                }
+                else{
+                    boolean isAllEpsilon = true;    //记录右侧的非终结符的First集合都包含ε
+                    //遍历产生式右部
+                    for(Symbol symbol : rightSymbols){
+                        if(symbol.isVt()){
+                            //该字符是终结符，则直接得到First集合
+                            firstSet.add(symbol);
+                            isAllEpsilon = false;
+                            break;
+                        }
+                        //如果是非终结符X，则递归调用这个函数
+                        //FIXME:对于左递归文法，如 A -> Ba, B -> A, 调用时会产生死循环
+                        FirstSet nextFirstSet = getFirstSet(symbol);
+                        firstSet.add(nextFirstSet.getSet());
+                        //如果X包含空，则继续往下一个字符推导
+                        if(!nextFirstSet.isHasEpsilon()){
+                            isAllEpsilon = false;
+                            break;
+                        }
+                    }
+                    if(isAllEpsilon){
+                        firstSet.setHasEpsilon(true);
+                    }
+                }
+            }
+            else{
+                firstSet.setHasEpsilon(true);   //First集为ε
+            }
+        }
+        //对形如A -> aAb的产生式进行特殊处理
+        for(Production production : badProductions){
+            List<Symbol> rightSymbols = production.getRight();
+            for(Symbol symbol : rightSymbols){
+                if(symbol.isVt()) {
+                    //是终结符
+                    firstSet.add(symbol);
+                    break;
+                }
+                if(symbol.equals(VNSymbol)){
+                    //形如A -> Aa 的形式
+                    if(!firstSet.isHasEpsilon())
+                        //如果A不能推出空，则该产生式是无效产生式，直接跳出
+                        break;
+                }
+                else{
+                    //形如A -> Ba 的形式
+                    FirstSet nextFirstSet = getFirstSet(symbol);
+                    firstSet.add(nextFirstSet.getSet());
+                    if(!nextFirstSet.isHasEpsilon())
+                        //如果B能推出空，则继续查找下一个字符，否则跳出
+                        break;
+                }
+            }
+        }
+        firstSetMap.put(VNSymbol,firstSet);
+        return firstSet;
+    }
+
     public Symbol getStart() {
         return start;
     }
@@ -93,10 +186,6 @@ public class Grammar {
 
     public Set<Symbol> getVnSet() {
         return VnSet;
-    }
-
-    public Map<Symbol, FirstSet> getFirstSetMap() {
-        return firstSetMap;
     }
 
     public Map<Symbol, FollowSet> getFollowSetMap() {
