455c1201b191999adc6e0e97edbe27d1cfb5c91f
ToSnow <towintersnow@gmail.com>
2022-04-17 21:19:14 +0800

    实现正规文法转NFA

--- src/com/compiler/parser/ParserUtils.java
+++ src/com/compiler/parser/ParserUtils.java
@@ -5,11 +5,13 @@ import com.compiler.model.Symbol;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ParserUtils {
-    public static List<Production> productionList;      //产生式列表
-
+    public final static List<Production> productionList = new ArrayList<>();      //产生式列表
+    public final static Map<Symbol,NFAState> nfaStateMap = new HashMap<>();     //当前非终结符对应的NFA结点
     /**
      * 读取正规文法，最后得到List<Production>的列表
      * @param filePath  正规文法的路径
@@ -20,7 +22,6 @@ public class ParserUtils {
             InputStreamReader reader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(reader);
             String parse = "";
-            productionList = new ArrayList<>();
             while(true){
                 try {
                     //读取每行的内容
@@ -69,6 +70,58 @@ public class ParserUtils {
         }
     }
 
+    /**
+     * 右线性正规文法转 NFA
+     * 1.增加终态结点，开始符号对应的结点作为初态
+     * 2.对形如A->b的文法，添加一条从A到终态的路径，路径为b
+     * 3.对形如A->bB的文法，添加一条从A到B的路径，路径为b
+     * */
+    public static void regularGrammarToNFA(){
+        //获取开始状态
+        if(productionList.size() == 0){
+            return;
+        }
+        //获取开始符号
+        Symbol startSymbol = productionList.get(0).getLeft();
+        //根据开始符号创建初态
+        NFAState startNFAState = new NFAState(startSymbol);
+        startNFAState.setStart(true);
+        nfaStateMap.put(startSymbol,startNFAState);
+        //创建终态
+        NFAState endState = new NFAState(true);
+        nfaStateMap.put(new Symbol("END STATE",true),endState);
+        //遍历产生式集合
+        for(Production production : productionList){
+            //先获取当前的NFAState
+            Symbol leftSymbol = production.getLeft();
+            NFAState currentNFAState = nfaStateMap.get(leftSymbol);
+            if(currentNFAState == null){
+                //没有则创建新的NFAState
+                currentNFAState = new NFAState(leftSymbol);
+                nfaStateMap.put(leftSymbol,currentNFAState);
+            }
+            if(production.getRight().size() == 1){
+                //形如A->b的文法
+                //连接到终态
+                currentNFAState.addEdges(production.getRight().get(0),endState);
+            }
+            else{
+                //形如A->bB的文法
+                //先获取两个文法符号
+                Symbol firstSymbol = production.getRight().get(0);
+                Symbol secondSymbol = production.getRight().get(1);
+                //获取下一个NFAState
+                NFAState nextNFAState = nfaStateMap.get(secondSymbol);
+                if(nextNFAState == null){
+                    nextNFAState = new NFAState(secondSymbol);
+                    nfaStateMap.put(secondSymbol,nextNFAState);
+                }
+                //添加边
+                currentNFAState.addEdges(firstSymbol,nextNFAState);
+            }
+        }
+    }
+
     public static void main(String[] args){
         String path = "src/com/compiler/parser/parse.txt";
         readParseTXT(path);
@@ -77,6 +130,6 @@ public class ParserUtils {
         for(Production production : productionList){
             System.out.println(production);
         }
-
+        regularGrammarToNFA();
     }
 }
